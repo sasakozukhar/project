913b084f1ad9a769264ef36818f3cf0b7fddf2bd
sasakozukhar <sasakozuhar93@gmail.com>
2024-10-29 19:44:36 +0200

    практикум 4

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task_1
+
+
+            //Console.WriteLine("Введите три числа для суммы:");
+            //Console.Write("Первое число:");
+
+            //2double c = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Второе число:");
+            //double d = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Третье число:");
+            //double e = Convert.ToDouble(Console.ReadLine());
+            //double a = c + d + e;
+            //Console.WriteLine($"Сумма трех чисел: {a}");
+
+            //Console.ReadKey();
+
+
+            // task_2
+
+
+            //Console.WriteLine("Введите длину первой стороны прямоугольника:");
+            //double side1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите длину второй стороны прямоугольника:");
+            //double side2 = Convert.ToDouble(Console.ReadLine());
+
+            //double perimeter = 2 * (side1 + side2);
+            //double area = side1 * side2;
+
+            //Console.WriteLine("Периметр прямоугольника: " + perimeter);
+            //Console.WriteLine("Площадь прямоугольника: " + area);
+
+            //Console.ReadKey();    
+
+
+
+            //task_3
+
+            //Console.WriteLine("Введите длину первого катета:");
+            //double cathet1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите длину второго катета:");
+            //double cathet2 = Convert.ToDouble(Console.ReadLine());
+
+            //double hypotenuse = Math.Sqrt(cathet1 * cathet1 + cathet2 * cathet2);
+
+            //Console.WriteLine("Гипотенуза треугольника: " + hypotenuse);
+
+            //Console.ReadKey();
+
+
+
+
+            //task_4
+
+            //Console.WriteLine("Введите длину стороны квадрата:");
+            //double side = Convert.ToDouble(Console.ReadLine());
+
+            //double perimeter = 4 * side;
+            //double area = side * side;
+
+            //Console.WriteLine("Периметр квадрата: " + perimeter);
+            //Console.WriteLine("Площадь квадрата: " + area);
+
+            //Console.ReadKey();
+        }
+    }
+}
+    
